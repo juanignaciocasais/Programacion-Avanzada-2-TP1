aa00545cbdac891a06590b0171359e8ee52f20d1
Juan I. Casais <juanignaciocasais@gmail.com>
2024-10-07 00:49:54 -0300

    Agrego Clase Response para el correcto manejo de respuestas

---
+++ src/main/java/com/example/TP1_AMQP/Response.java
@@ -0,0 +1,20 @@
+package com.example.TP1_AMQP;
+
+public class Response {
+
+    private String message;
+    private Object data;
+
+    public Response(String message, Object data) {
+        this.message = message;
+        this.data = data;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public Object getData() {
+        return data;
+    }
+}
